ae7e330d8839e6aefc27b49d238bfc14b9373684
Magno <42675117+magnomoreira@users.noreply.github.com>
2021-08-10 22:56:13 -0300

    Creating classe Produto (#1)

---
+++ Product.ApiRest/Model/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product.ApiRest.Model
+{
+	[Table("produto")]
+	public class Produto
+	{
+		[Key]
+		[Column("id")]
+		public int Id { get; set; }
+		[Column("name")]
+
+		public string Name { get; set; }
+		[Column("type")]
+
+		public string  Type { get; set; }
+		[Column("enable")]
+
+		public bool Enable { get; set; }
+	}
+}
