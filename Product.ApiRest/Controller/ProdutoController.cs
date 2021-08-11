92ac160feb435c67db5830e41a21f2d58b22647c
Magno Moreira <magnomoreira@yahoo.com>
2021-08-11 09:09:46 -0300

    Creating controller

---
+++ Product.ApiRest/Controller/ProdutoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product.ApiRest.Controller
+{
+	[ApiController]
+	[Controller]
+	public class ProdutoController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult FindAll()
+		{
+			return Ok();
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult FindById(int id)
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		public IActionResult Create()
+		{
+			return Ok();
+		}
+		
+		[HttpPut("{id}")]
+		public IActionResult Update(int id )
+		{
+			return Ok();
+		}
+
+		[HttpPatch("{id}")]
+
+		public IActionResult Patch(int id)
+		{
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delite(int id)
+		{
+			return Ok();
+		}
+	}
+}
