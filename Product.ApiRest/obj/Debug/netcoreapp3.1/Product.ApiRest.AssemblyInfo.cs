e6f4b9d02297ae994ebe8cbc21b627ad8ce92a0e
Magno <42675117+magnomoreira@users.noreply.github.com>
2021-08-13 15:51:24 -0300

    Revert "Implementando o docker na aplicação (#3)"
    This reverts commit efa99744e5be0d72a8e3c2eb43311af9b74b7fd3.

--- Product.ApiRest/obj/Debug/netcoreapp3.1/Product.ApiRest.AssemblyInfo.cs
+++ Product.ApiRest/obj/Debug/netcoreapp3.1/Product.ApiRest.AssemblyInfo.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("dca82916-f979-410c-807f-71402d821179")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.ApiRest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
